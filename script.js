2ccea1047a2ba2daced8f54d3caf7816212d70e0
TanishkaMandil-07 <tanishkamandil4@gmail.com>
2025-12-26 09:24:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,103 @@
+let h1=document.querySelector("h1");
+h1.addEventListener("mouseover",()=>{
+    h1.innerText="You Look Good";
+    h1.style.fontSize="1.5vw";
+});
+h1.addEventListener("mouseleave",()=>{
+    h1.innerText="Glossier."
+    h1.style.fontSize="2vw";
+});
+let img1 = document.querySelector("#box1-1 img");
+
+img1.addEventListener("mouseover", () => {
+    img1.src = "Earbuds Beauty Bag.jpg";
+});
+
+img1.addEventListener("mouseleave", () => {
+    img1.src = "https://glossier-prod.imgix.net/files/glossier-holiday-earbud-case-carousel-01.png";
+});
+let img2=document.querySelector("#box2-1 img");
+  img2.addEventListener("mouseover",()=>{
+    img2.src="https://glossier-prod.imgix.net/files/glossier-lv-babytee-ugc-02.png?auto=compress,format&cs=srgb&w=720";
+  });
+   img2.addEventListener("mouseleave",()=>{
+    img2.src="https://glossier-prod.imgix.net/files/glossier-lv-babytee-carousel-01.png?auto=compress,format&cs=srgb&w=1374?auto=compress,format&cs=srgb";
+  });
+let img3=document.querySelector("#box3-1 img");
+    img3.addEventListener("mouseover",()=>{
+    img3.src="https://glossier-prod.imgix.net/files/glossier-lasvegas-boxer-ugc-01.png?auto=compress,format&cs=srgb&w=720";
+});
+img3.addEventListener("mouseleave",()=>{
+    img3.src="https://glossier-prod.imgix.net/files/glossier-lasvegas-boxer-carousel-01.png?auto=compress,format&cs=srgb&w=1374?auto=compress,format&cs=srgb";
+});
+let img4=document.querySelector("#box4-1 img");
+    img4.addEventListener("mouseover",()=>{
+    img4.src="https://glossier-prod.imgix.net/files/glossier-london-mug-UGC-01.png?auto=compress,format&cs=srgb&w=720";
+});
+img4.addEventListener("mouseleave",()=>{
+    img4.src="https://glossier-prod.imgix.net/files/glossier-london-mug-carousel-01.png?auto=compress,format&cs=srgb&w=1374?auto=compress,format&cs=srgb";
+});
+let button1=document.querySelector("#page1-heading button");
+let img5=document.querySelector("#box2_img img");
+img5.addEventListener("mouseover",()=>{
+    img5.src="img5.png";
+});
+img5.addEventListener("mouseleave",()=>{
+    img5.src="https://glossier-prod.imgix.net/files/glossier-holiday-sugar-spice-carousel-01.png?auto=compress,format&cs=srgb&w=1374?auto=compress,format&cs=srgb";
+});
+let img6=document.querySelector("#box22_img img");
+img6.addEventListener("mouseover",()=>{
+    img6.src="https://glossier-prod.imgix.net/files/glossier-after-baume-carousel-08_496c8d58-d3e7-49f7-8c6a-9cc7e35e63f8.png?auto=compress,format&cs=srgb&w=720";
+})
+img6.addEventListener("mouseleave",()=>{
+    img6.src="https://glossier-prod.imgix.net/files/glossier-skincare-afterbaume-carousel-01.png?auto=compress,format&cs=srgb&w=1374?auto=compress,format&cs=srgb";
+})
+let img7=document.querySelector("#box23_img img");
+img7.addEventListener("mouseover",()=>{
+    img7.src="https://glossier-prod.imgix.net/files/glossier-futuredew-09.png?auto=compress,format&cs=srgb&w=1374";
+})
+img7.addEventListener("mouseleave",()=>{
+    img7.src="https://glossier-prod.imgix.net/files/glossier-skincare-futuredew-carousel-01.png?auto=compress,format&cs=srgb&w=1374?auto=compress,format&cs=srgb";
+})
+let img8=document.querySelector("#box24_img img");
+img8.addEventListener("mouseover",()=>{
+    img8.src="https://glossier-prod.imgix.net/files/glossier-holiday-cp-trio-carousel-12.png?auto=compress,format&cs=srgb&w=720";
+})
+img8.addEventListener("mouseleave",()=>{
+    img8.src="https://glossier-prod.imgix.net/files/glossier-holiday-cp-trio-carousel-01.png?auto=compress,format&cs=srgb&w=1374?auto=compress,format&cs=srgb";
+});
+let img9=document.querySelector("#page3-img img");
+img9.addEventListener("mouseover",()=>{
+    img9.src="Our Skin Story.jpg";
+});
+img9.addEventListener("mouseleave",()=>{
+    img9.src="https://glossier-prod.imgix.net/files/glossier-skincare-set-tout-desktop.png?auto=compress,format&cs=srgb&w=1080";
+});
+let img10=document.querySelector("#page32-img img");
+img10.addEventListener("mouseover",()=>{
+    img10.src="KATSEYE LARA X GLOSSIER.jpg";
+});
+img10.addEventListener("mouseleave",()=>{
+    img10.src="Glossier.jpg";
+});
+let img11=document.querySelector("#box5_1img img");
+img11.addEventListener("mouseover",()=>{
+    img11.src="https://glossier-prod.imgix.net/files/glossier-cppb-puff-carousel-12.png?auto=compress,format&cs=srgb&w=720";
+})
+img11.addEventListener("mouseleave",()=>{
+    img11.src="https://glossier-prod.imgix.net/files/glossier-cppb-puff-carousel-01.png?auto=compress,format&cs=srgb&w=1377?auto=compress,format&cs=srgb";
+})
+let img12=document.querySelector("#box5_2img img");
+img12.addEventListener("mouseover",()=>{
+    img12.src="https://glossier-prod.imgix.net/files/glossier-boybrow-review-highlight.png?auto=compress,format&cs=srgb&w=1296";
+})
+img12.addEventListener("mouseleave",()=>{
+    img12.src="https://glossier-prod.imgix.net/files/glossier-boy-brow-dark-brown-carousel-01.png?auto=compress,format&cs=srgb&w=1374?auto=compress,format&cs=srgb";
+})
+let img13=document.querySelector("#box5_3img img");
+img13.addEventListener("mouseover",()=>{
+    img13.src="Screenshot 2025-12-25 170804.png";
+})
+img13.addEventListener("mouseleave",()=>{
+    img13.src="https://glossier-prod.imgix.net/products/glossier-monochromes-clay-carousel-01.png?auto=compress,format&cs=srgb&w=916?auto=compress,format&cs=srgb";
+})
